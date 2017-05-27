79ac94ce4b93f0795b77412122f7c15107cb903a
austin stone <stone@stone-rmbp.local>
2017-05-27 16:52:49 +0800

    basic syntax

---
+++ README.md
@@ -0,0 +1,3 @@
+# StudyKotlin
+
+The most samples from [kotlin-docs](http://kotlinlang.org/docs/kotlin-docs.pdf).
---
+++ src/HelloWorld.kt
@@ -0,0 +1,15 @@
+/**
+ * desc  :
+ * author: stone
+ * email : aa86799@163.com
+ * time  : 27/05/2017 10 25
+ */
+
+fun main(args: Array<String>) {
+    print("Hello World\n")
+}
+
+
+
+
+
