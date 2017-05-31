4f4689ace95f73980e9a9dd8d9c5f389b7e08d81
austin stone <stone@stone-rmbp.local>
2017-05-31 15:36:36 +0800

    package and import

---
+++ KotlinFeature/src/com/stone/basic/packages/Info.kt
@@ -0,0 +1,40 @@
+package com.stone.basic.packages
+import kotlin.*
+//import static java.lang.Math  //Kotlin 不支持
+/**
+ * desc  :
+ * author: stone
+ * email : aa86799@163.com
+ * time  : 31/05/2017 10 04
+ */
+
+/*
+默认import Kotlin file ：
+— kotlin.*
+— kotlin.annotation.*
+— kotlin.collections.*
+— kotlin.comparisons.* (since 1.1)
+— kotlin.io.*
+— kotlin.ranges.*
+— kotlin.sequences.*
+— kotlin.text.*
+
+还有 Kotlin— JVM:
+— java.lang.*
+— kotlin.jvm.*
+
+//Kotlin 不支持 静态方法导入
+ */
+
+private fun HAA() {
+    checkNotNull("")
+}
+
+//protected fun HBB() {// protected不能用顶级函数声明
+//
+//}
+
+fun HCC() {//public
+
+}
+
